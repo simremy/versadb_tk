d16c1cefb52e6e8f72d0ab32e9642baacc5570c8
Simon Remy <41745996+simremy@users.noreply.github.com>
2023-06-05 16:23:07 +0200

    Update rdkit_creat.bat

--- NMRshift/rdkit_creat.bat
+++ NMRshift/rdkit_creat.bat
@@ -1,5 +1,5 @@
 @call conda activate my-rdkit-env-lotus
 @echo %~dp0
-@cd "C:\Users\%username%\Desktop\versadb_tk-main\NMRshift\"
+@cd "C:\Users\%username%\Desktop\versadb_tk-master\NMRshift\"
 @python process.py
 
